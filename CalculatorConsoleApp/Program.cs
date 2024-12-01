b876f65c4d75b2156736238cb51f1bbfde9be6c9
Jono Cabrera <cjonathan193@gmail.com>
2024-12-01 18:28:22 +1100

    Add project files.

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
